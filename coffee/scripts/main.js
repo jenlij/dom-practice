e771b792d32f32731cf47f563fafe8e032453685
jenlij <jljohns1216@gmail.com>
2017-07-24 15:08:35 -0400

    prints queue to screen in bluck for page refresh

--- coffee/scripts/main.js
+++ coffee/scripts/main.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
         //add function here to push to order queue
         addOrderToQueue(myOrder, 'orderQueue');
         //print order queue
-        printOrderQueue('orderQueue');
+        printMyOrder(myOrder);
         event.preventDefault();
     });
     //before refresh
@@ -19,13 +19,13 @@ $(document).ready(function(){
 
 //after refresh page
 window.onload = function() {
-        var order = getFromLocalStorage('myOrder');
-        $('#coffeeOrder').val(order['coffee']);
-        $('#emailInput').val(order['emailAddress']);
-        $('#' + order['size']).prop('checked', true);
-        $('#flavorShot').val(order['flavor']);
-        $('#strengthLevel').val(order['strength']);
-        
+    var order = getFromLocalStorage('myOrder');
+    $('#coffeeOrder').val(order['coffee']);
+    $('#emailInput').val(order['emailAddress']);
+    $('#' + order['size']).prop('checked', true);
+    $('#flavorShot').val(order['flavor']);
+    $('#strengthLevel').val(order['strength']);
+    printOrderQueue('orderQueue');        
 }
 
 function saveToLocalStorage(arr, orderName) {
@@ -63,19 +63,38 @@ function addOrderToQueue(myOrder, orderQueueName) {
 function printOrderQueue(orderQueueName) {
     var $table = $('[name=tableBody]');
     var orderQueue = getFromLocalStorage(orderQueueName);
-    var $tr = $('<tr>');
+    var $tr = '';
     var $email = '';
     var $coffee = '';
     var $size = '';
     var $flavor = '';
     var $strength = '';
     Object.keys(orderQueue).forEach(function(key){
+        $tr = $('<tr>');
         $email = $('<td>' + orderQueue[key]['emailAddress'] + '</td>');
         $coffee = $('<td>' + orderQueue[key]['coffee'] + '</td>');
         $size = $('<td>' + orderQueue[key]['size'] + '</td>');
         $flavor = $('<td>' + orderQueue[key]['flavor'] + '</td>');
         $strength = $('<td>' + orderQueue[key]['strength'] + '</td>');
+        $tr.append($email).append($coffee).append($size).append($flavor).append($strength);
+        $table.append($tr);
     });
+}
+
+function printMyOrder(myOrder) {
+    var $table = $('[name=tableBody]');
+    var $tr = '';
+    var $email = '';
+    var $coffee = '';
+    var $size = '';
+    var $flavor = '';
+    var $strength = '';
+    $tr = $('<tr>');
+    $email = $('<td>' + myOrder['emailAddress'] + '</td>');
+    $coffee = $('<td>' + myOrder['coffee'] + '</td>');
+    $size = $('<td>' + myOrder['size'] + '</td>');
+    $flavor = $('<td>' + myOrder['flavor'] + '</td>');
+    $strength = $('<td>' + myOrder['strength'] + '</td>');
     $tr.append($email).append($coffee).append($size).append($flavor).append($strength);
     $table.append($tr);
 }
